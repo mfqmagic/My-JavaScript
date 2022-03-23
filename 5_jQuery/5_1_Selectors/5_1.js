bbcdc77d26891bd565ef369d2bd9e549bed2e072
mfqmagic <mfqmagic@gmail.com>
2022-03-23 18:08:31 +0900

    jQuery 5_1

---
+++ 5_jQuery/5_1_Selectors/5_1.js
@@ -0,0 +1,14 @@
+// DOMの読み込みが終わったら
+$(document).ready(function () {
+    // TODO 「全て」のクリックイベントを追加
+    $("#all").click(function () {
+
+    });
+
+    // 各項目にクリックイベントを追加
+    $("#list :checkbox").click(function () {
+        // TODO 各項目全て選択される場合、「全て」も選択
+
+    });
+
+});
